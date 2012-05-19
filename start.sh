8505c6f44f83db80106460201b04bd0bc38626d1
Garrett Heath Koller <garrettheath4@gmail.com>
2012-05-19 09:05:08 +0000

    Start script detatches from screens

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-screen -S pointServer python pointServer.py
+screen -d -m -S pointServer python pointServer.py
 
 sleep 2
 
-screen -S bus1 python simulateBus1.py
-screen -S bus2 python simulateBus2.py
-screen -S bus3 python simulateBus3.py
-screen -S bus4 python simulateBus4.py
+screen -d -m -S bus1 python simulateBus1.py
+screen -d -m -S bus2 python simulateBus2.py
+screen -d -m -S bus3 python simulateBus3.py
+screen -d -m -S bus4 python simulateBus4.py
