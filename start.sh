79e28c92983ba4977eea4315250095cc165c5d7a
Garrett Heath Koller <kollerg14@mail.wlu.edu>
2012-05-19 04:51:44 -0400

    Added simulators to start and stop scripts

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
 python pointServer.py &
+
+sleep 2
+
+python simulateBus1.py &
+python simulateBus2.py &
+python simulateBus3.py &
+python simulateBus4.py &
--- stop.sh
+++ stop.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
 kill `ps aux | fgrep 'python pointServer.py' | grep -v fgrep | awk '{ print $2 }'`
+
+kill `ps aux | fgrep 'python simulateBus1.py' | grep -v fgrep | awk '{ print $2 }'`
+kill `ps aux | fgrep 'python simulateBus2.py' | grep -v fgrep | awk '{ print $2 }'`
+kill `ps aux | fgrep 'python simulateBus3.py' | grep -v fgrep | awk '{ print $2 }'`
+kill `ps aux | fgrep 'python simulateBus4.py' | grep -v fgrep | awk '{ print $2 }'`
